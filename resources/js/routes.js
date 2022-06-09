55fb2b84572e896ccb27bc8ad5aa0de58fa9288c
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-09 22:17:54 +0200

    Update FE routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,21 @@
 import Dashboard from "./components/Pages/Dashboard";
 import Posts from "./components/Pages/Posts";
 import PageNotFound from "./components/Pages/PageNotFound";
+import User from "./components/Pages/User";
+import DoNotDisturb from "./components/Pages/DoNotDisturb";
 
 
 const routes = [
     { path: '/', component: Dashboard },
-    { path: 'posts', component: Posts },
+    { path: '/posts', component: Posts },
+    { path: '/users/:id', component: User },
+    {
+        path: '/settings/do-not-disturb',
+        component: DoNotDisturb,
+        props: {
+            actionUrl: `${process.env.MIX_APP_URL}/api/settings/do-not-disturb`,
+        },
+    },
     // { path: '/', component: Dashboard },
     {
         path: '*',
