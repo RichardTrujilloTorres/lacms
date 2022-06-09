cf9721abfaaf95523c790b943925f6576fbf11f8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-09 22:15:27 +0200

    Add notification model and its migration

---
+++ app/Models/Notification.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    const DEFAULT_LIMIT = 10;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+}
