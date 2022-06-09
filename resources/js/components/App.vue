0434a3abdb1e45906d90902c9b13e1ce2ecf73e2
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-09 22:18:33 +0200

    Add notifications url prop on navbar component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,7 +3,7 @@
         <common_sidebar></common_sidebar>
 
         <div class="main-panel">
-            <common_navbar></common_navbar>
+            <common_navbar :notifications-url="notificationsUrl"></common_navbar>
             <router-view></router-view>
             <common_footer></common_footer>
         </div>
@@ -12,10 +12,14 @@
 </template>
 
 <script>
-import VueRouter from "vue-router";
     export default {
+        props: {
+            notificationsUrl: {
+                type: String,
+                required: true,
+            },
+        },
         mounted() {
-            console.log('Component mounted.')
         }
     }
 </script>
