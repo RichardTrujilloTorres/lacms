43f5545d08907706e7520bc58a4b15c3a85c020d
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-09 18:31:28 +0200

    Set app as an SPA

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{any}', [\App\Http\Controllers\SPAController::class, '__invoke'])
+    ->where('any', '.*')
+    ->name('app');
+
+//Auth::routes();
+
