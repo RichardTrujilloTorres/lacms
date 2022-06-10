9900ccd4c46ac02d433d96fefe66c0a0d50edef0
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-10 03:23:16 +0200

    Add success message on "do not disturb" status update

--- resources/js/components/Pages/DoNotDisturb.vue
+++ resources/js/components/Pages/DoNotDisturb.vue
@@ -61,6 +61,7 @@ export default {
             })
             .then(res => {
                 console.log(res.data)
+                this.$toasted.show(`Status updated.`, {duration: 3000})
             })
             .catch(err => {
                 console.log(err)
