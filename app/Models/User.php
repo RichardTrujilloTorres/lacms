d7fe67b7e3edeb0651f92ed058b0b4738e7bef27
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-11 11:08:26 +0200

    Add user formatted email verified at attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -41,4 +42,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = [
+        'formattedEmailVerifiedAt',
+    ];
+
+    public function getFormattedEmailVerifiedAtAttribute(): string
+    {
+        return Carbon::parse($this->email_verified_at)->format('d-m-Y H:i');
+    }
 }
