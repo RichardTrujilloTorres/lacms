0926029e42c41e76bafe2454ec2b909d985b2df0
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-06-11 08:42:12 +0200

    Add setting constants

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -12,6 +12,12 @@ class Setting extends Model
     const DO_NOT_DISTURB_STATUS_ON = 'on';
     const DO_NOT_DISTURB_STATUS_OFF = 'off';
 
+    const SYNC_STATUS_UP_TO_DATE = 'up-to-date';
+    const SYNC_STATUS_NOT_UP_TO_DATE = 'not-up-to-date';
+
+    const DO_NOT_DISTURB_KEY = 'do_not_disturb';
+    const SYNC_KEY = 'sync';
+
     protected $fillable = [
         'name',
         'definition',
